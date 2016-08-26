ef750601bb75ce07f3ed0bfb0ecce33dac954afb
James Jarvis <jj@aprsworld.com>
2016-08-26 22:00:26 +0000

    Bugfix: netconfig-scan.php...

--- netconfig-scan.php
+++ netconfig-scan.php
@@ -1,31 +1,37 @@
 <?php
 
 function iwlist_parse_scan ($s) {
-	$interfaces_strings = split("\n\n", $s);
+	$interfaces_strings = explode("\n\n", $s);
 	$ret = Array();
 
 	foreach ($interfaces_strings as $interface_string) {
 		$lines = split("\n", $interface_string);
 		$iface = split(" ", $lines[0])[0];
-		$ssid = NULL;
-		$auth = NULL;
-		preg_match('/ESSID:"([\w]+)"/', $interface_string, $matches);
-		if (sizeof($matches) > 0) {
-			$ssid = $matches[1];
-		} else {
-			continue;
-		}
-		preg_match('/Encryption key:on/', $interface_string, $matches);
-		if (sizeof($matches) > 0) {
-			$auth = true;
-		} else {
-			$auth = false;
-		}
-		// TODO: Encryption and Authentication info...
 
-		$ret[$iface] = Array();
-		$ret[$iface][$ssid] = Array();
-		$ret[$iface][$ssid]['auth'] = $auth;
+		$scans = explode("Cell", $interface_string);
+		foreach ($scans as $scan) {
+			$ssid = NULL;
+			$auth = NULL;
+			preg_match('/ESSID:"([\w]+)"/', $scan, $matches);
+			if (sizeof($matches) > 0) {
+				$ssid = $matches[1];
+			} else {
+				continue;
+			}
+			preg_match('/Encryption key:on/', $scan, $matches);
+			if (sizeof($matches) > 0) {
+				$auth = true;
+			} else {
+				$auth = false;
+			}
+			// TODO: Encryption and Authentication info...
+
+			if (!array_key_exists($iface, $ret)) {
+				$ret[$iface] = Array();
+			}
+			$ret[$iface][$ssid] = Array();
+			$ret[$iface][$ssid]['auth'] = $auth;
+		}
 	}
 
 	return $ret;
