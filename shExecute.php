2e8c91b11c905043850290fd57430daab4864dba
Cdetviler <charlie@aprsworld.com>
2016-10-06 21:47:37 +0000

    php shExecute now takes a get parameter (will be post in final release), and decided which script to run based on that param. The final implementation will allow for an ajax call and will return a json responsew

--- shExecute.php
+++ shExecute.php
@@ -1,6 +1,22 @@
 <?php
-$oldDir = getcwd();
-chdir('/tmp/');
-exec('sudo ./sampleScript.sh');
-chdir($oldDir);
+
+
+function toggleFeature($feature){
+  $oldDir = getcwd();
+  chdir('/tmp/');
+  switch($feature) {
+    case 'test1':
+      exec('sudo ./sampleScript.sh');
+      break;
+    case 'test2':
+      exec('sudo ./sampleScript1.sh');
+      break;
+    default:
+      echo 'no feature by that name';
+  }
+  chdir($oldDir);
+}
+
+toggleFeature($_GET['feature']);
+
 php?>
